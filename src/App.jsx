df9778180a6a6f204cfb3272923d1f9ccb842467
Mohammed Maaz Rayeen <mmaaz0510@gmail.com>
2025-03-10 08:59:43 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main className="bg-black">
+        <h1>
+            Hello World
+        </h1>
+    </main>
+  )
+}
+
+export default Sentry.withProfiler(App);
